c7a9809b250ae9162e840aeb6eb757e604289ab9
MASTHAN5 <vh12613_mech22@velhightech.com>
2025-01-22 20:25:27 +0530

    Create sub.java
    In this program, store an integer in a variable and perform subtraction and addition tasks between the two variable

---
+++ sub.java
@@ -0,0 +1,14 @@
+//SUBTRACTION IN JAVA
+public class sub{
+  public static void main(String args[])
+  {
+    int a = -10;
+    int b = -29;
+    int c = -46;
+    System.out.println(a-b-c);
+    System.out.println(a-b+c);
+  }
+}
+//output:
+//65
+//-27
