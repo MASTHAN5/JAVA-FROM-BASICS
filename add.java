37bdb138b4ea838d07ff7e44b300d50953330568
MASTHAN5 <vh12613_mech22@velhightech.com>
2025-01-22 19:55:50 +0530

    Create add.java
    In this program, store an integer in a variable and perform addition and subtraction tasks between the three variable

---
+++ add.java
@@ -0,0 +1,16 @@
+// addition in java
+public class add{
+  public static void main(String args[])
+  {
+    //integers stored in a variable
+    int a = 23;//integers stored in a variable
+    int b = 49;
+    int c = 72;
+    System.out.println(a+b+c);
+    System.out.println(a-b-c);
+    System.out.println(a+b-c);
+  }
+}
+//output: 144
+//        -98
+//         0
