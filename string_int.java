7e3ff8afe764322b01d7292b862b9aea69b7acd8
MASTHAN5 <vh12613_mech22@velhightech.com>
2025-01-22 19:12:52 +0530

    Create string_int.java
    storing the name and phone number in the variable of a person using datatypes (string=name , int=phone number)

---
+++ string_int.java
@@ -0,0 +1,9 @@
+public class string_int{
+  public static void main(String args[])
+{
+  String Name = "John";
+  int Phone_number = 1234567890;
+    System.out.println(Name);
+    System.out.println(Phone_number);
+}
+}
