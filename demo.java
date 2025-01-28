fbd30026aaf044900b1f182b1195991cec766033
MASTHAN5 <vh12613_mech22@velhightech.com>
2025-01-28 19:23:08 +0530

    Create demo.java
    Working in if and else statements, we can get the answer in output depending on whether the boolean value is true or false.

---
+++ demo.java
@@ -0,0 +1,18 @@
+//if and else statement
+public class demo {
+    public static void main(String[] args)
+    {
+        boolean rain = false; //store true or false in boolean to get the decision
+        if(rain){
+            System.out.println("Take an umbrella"); //scenario 1
+        }
+        else{
+            System.out.println("Enjoy the sunlight"); //scenario 2
+        }
+    }
+    
+}
+
+//output
+//if it is true "Take an umbrella"
+//else "Enjoy the sunlight"
