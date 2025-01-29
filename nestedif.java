097f92c4027374e72aadeca7d1f63cd94c330e92
MASTHAN5 <vh12613_mech22@velhightech.com>
2025-01-30 00:23:08 +0530

    Create nestedif.java
    nested if statement

---
+++ nestedif.java
@@ -0,0 +1,21 @@
+//nested if 
+public class nestedif {
+    public static void main(String[] args){
+        boolean kfc = true;
+        boolean chicken = true;
+        boolean pepsi = true;
+        if(kfc){
+            System.out.println("get in");
+            if(chicken){
+               System.out.println("watch the menu");
+               if(pepsi){
+                System.out.println("order your food with two pepsi");
+               }
+        else{
+            System.out.println("exit the shop");
+        }
+            }
+        }}
+
+    
+}
