c23f6069ae7279523e965ef6a0cb98b8e7ba2aa8
MASTHAN5 <vh12613_mech22@velhightech.com>
2025-01-30 00:03:58 +0530

    Create elseif.java
    mark separation using else if.

---
+++ elseif.java
@@ -0,0 +1,18 @@
+//else if 
+public class elseif {
+    public static void main(String[] args)
+    {
+        int task= 89 //input
+        ;
+        if(task<=30 || task<60){
+            System.out.println("Video game"); 
+        }
+        else if(task<=60 || task<90){
+            System.out.println("iphone");
+        }
+        else if(task>=90){
+            System.out.println("mac book pro");
+        }
+    }
+    
+}
