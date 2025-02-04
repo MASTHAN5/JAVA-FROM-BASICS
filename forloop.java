0c3970b2ecc8782728439eb286507c4177970fa8
MASTHAN5 <vh12613_mech22@velhightech.com>
2025-02-04 07:03:03 +0530

    Create forloop.java
    repeating using for loop.

---
+++ forloop.java
@@ -0,0 +1,8 @@
+public class forloop { //forloop
+    public static void main(String[] args){
+        for(int i=1;i<=10;i=i+1){ //looping
+            System.out.println("JOHN");
+            }
+    
+}
+}
