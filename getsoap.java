bd7eaa642c69e993282cb0a4c6e0be3c6855bbed
MOHAMED KHALESHA MASTHAN A <vh12613_mech22@velhightech.com>
2025-02-13 02:40:08 +0530

    Create getsoap.java
    Functions with parameters

---
+++ getsoap.java
@@ -0,0 +1,13 @@
+//functions with parameters
+public class getsoap {
+	void store(int money){
+	System.out.println(money);
+	System.out.println("Get soap");
+	}
+	public static void main(String[] args) {
+		getsoap obj1 = new getsoap();
+		obj1.store(20);
+		
+	}
+
+}
