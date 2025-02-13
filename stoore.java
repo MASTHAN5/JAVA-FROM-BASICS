89f1c04084a18dddd6e9440ac1e8b0b97498bf84
MOHAMED KHALESHA MASTHAN A <vh12613_mech22@velhightech.com>
2025-02-14 01:43:12 +0530

    Create stoore.java
    parameters

---
+++ stoore.java
@@ -0,0 +1,13 @@
+public class stoore {
+	int getsoap(int money) {
+		int soap_price = 18;
+		int rem =20-18;
+		return rem;
+	}
+	public static void main(String[] args) {
+		stoore obj1= new stoore();
+		int remainder = obj1.getsoap(20);
+		System.out.println(remainder);
+	}
+
+}
