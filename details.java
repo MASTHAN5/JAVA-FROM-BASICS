dcd7109cc232f37764750fd314723cddd987aebe
MOHAMED KHALESHA MASTHAN A <vh12613_mech22@velhightech.com>
2025-02-15 01:47:58 +0530

    Create details.java

---
+++ details.java
@@ -0,0 +1,13 @@
+public class details {
+	String getname() 
+	{
+		return "mkm";
+	}
+	
+	public static void main(String[] args) {
+		details obj1 = new details();
+		String myname= obj1.getname();
+		System.out.println(myname);
+	}
+
+}
