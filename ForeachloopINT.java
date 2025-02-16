366fcf008e7ab3017ef50efc59ccc4f626238919
MOHAMED KHALESHA MASTHAN A <vh12613_mech22@velhightech.com>
2025-02-17 01:37:17 +0530

    Create ForeachloopINT.java
    Enhanced for loop integer.

---
+++ ForeachloopINT.java
@@ -0,0 +1,16 @@
+//enhanced for loop(INT)
+public class ForeachloopINT {
+	 public static void main(String[] args) {
+		 int num[] = {10,20,30,40};
+		 for(int i=0;i<=3;i++) {
+			 System.out.println(num[i]);
+		 }
+		 for (int var:num) {
+			 System.out.println(var);
+		 
+			 
+		 }
+		 
+	 }
+}
+
