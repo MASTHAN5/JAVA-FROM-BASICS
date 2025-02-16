fa8cc3a2f910f536efbc29ff4c8a8b25079cb5d6
MOHAMED KHALESHA MASTHAN A <vh12613_mech22@velhightech.com>
2025-02-17 01:40:32 +0530

    Create ForeachloopString.java
    Enhanced for loop for string.

---
+++ ForeachloopString.java
@@ -0,0 +1,13 @@
+//enhanced for loop(String)
+public class ForeachloopString {
+	 public static void main(String[] args) {
+		 String dum[] = {"one", "two"};
+		 
+		 for (String var:dum) {
+			 System.out.println(var);
+			 
+		 }
+		 
+	 }
+}
+
