28a99c092c41147884abdbc5e27acd7f0136d9a5
MOHAMED KHALESHA MASTHAN A <vh12613_mech22@velhightech.com>
2025-02-18 23:47:55 +0530

    Create Constructor.java
    Constructor in java

---
+++ Constructor.java
@@ -0,0 +1,17 @@
+public class Constructor {
+	
+	int numb;
+	String name;
+	
+	Constructor(){
+		System.out.println("Hello");
+		
+	}
+
+	public static void main(String[] args) {
+		Constructor obj1 = new Constructor();
+		
+
+	}
+
+}
