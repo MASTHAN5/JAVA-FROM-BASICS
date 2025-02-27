ea0cdd29f9beb00cb8b92fe62c93a496a7e1f1fe
MOHAMED KHALESHA MASTHAN A <vh12613_mech22@velhightech.com>
2025-02-27 23:52:43 +0530

    Create Operators.java

---
+++ Operators.java
@@ -0,0 +1,15 @@
+public class Operators {
+    public static void main(String[] args) {
+        int x = 7, y = 3;
+        System.out.println(x + y);
+        System.out.println(x - y);
+        System.out.println(x * y);
+        System.out.println(x / y);
+        System.out.println(x % y);
+        System.out.println(x > y);
+        System.out.println(x == y);
+        System.out.println((x > 5) && (y < 5));
+        System.out.println(x | y);
+        System.out.println(x >> 1);
+    }
+}
