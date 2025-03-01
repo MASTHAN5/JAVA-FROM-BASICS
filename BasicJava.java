3f4ec12baeed1548636b6c0bbf6713b2e1e1b288
MOHAMED KHALESHA MASTHAN A <vh12613_mech22@velhightech.com>
2025-03-02 00:20:11 +0530

    Create BasicJava.java

---
+++ BasicJava.java
@@ -0,0 +1,13 @@
+public class BasicJava {
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 5;
+        int sum = num1 + num2;
+        System.out.println("Sum: " + sum);
+        if (num1 > num2) {
+            System.out.println("num1 is greater than num2");
+        } else {
+            System.out.println("num2 is greater than or equal to num1");
+        }
+    }
+}
