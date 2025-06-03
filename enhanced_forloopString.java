f4dfa33b68e13a856cd40ab6eb4117cf67e88208
MOHAMED KHALESHA MASTHAN A <vh12613_mech22@velhightech.com>
2025-06-04 01:34:00 +0530

    Create enhanced_forloopString.java
    Enhanced forloop method

---
+++ enhanced_forloopString.java
@@ -0,0 +1,8 @@
+public class enhanced_forloopString{
+  public static void main(String[] args){
+    String[] cum = {"one", "two"};
+    for(String dum:cum){
+      System.out.println(dum);
+    }
+  }
+}
